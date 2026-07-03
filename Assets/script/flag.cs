aaaf32356ad681ec24d44706d0be3b724f0b9fbb
スカイ <2247032@ecc.ac.jp>
2026-07-03 14:46:43 +0900

    test

--- Assets/script/flag.cs
+++ Assets/script/flag.cs
@@ -3,16 +3,20 @@ using UnityEngine.UIElements;
 
 public class flag : MonoBehaviour
 {
-    public static bool[] clear= { false };
+    //public static bool[] clear = new bool[maxscene];
+    public static bool[] clear;
     public int gomi=0;
     public int okimono=0;
     public bool kakunin;
     public static int scene=0;
     public int nowscene;
+    public static int maxscene=40;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         scene=nowscene;
+        clear = new bool[maxscene];
     }
 
     // Update is called once per frame
@@ -21,6 +25,7 @@ public class flag : MonoBehaviour
         if (gomi==0 && okimono==0)
             clear[scene] = true;
         kakunin = clear[scene];
+        //kakuninscene=scene; 
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
